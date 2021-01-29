5d8391a38ef377647e557bfa7bd997523f1deed9
Catherine Shaw <cshaw1382@gmail.com>
2021-01-29 16:02:35 -0500

    update to show past, present and future

--- script.js
+++ script.js
@@ -6,19 +6,26 @@ console.log(currentDay);
 console.log(currentHr);
 
 var blockHr = document.querySelectorAll(".time-block")
+console.log(blockHr)
+
 
 for( i=0; i < blockHr.length; i++ ){
-if (blockHr < currentHr){
+blockHr.textContent = i
+console.log(blockHr.textContent)
+if ( (8 + (i)) < currentHr ){
     blockHr[i].classList.add("past")
+    console.log('past')
     }
-    else if (blockHr === currentHr) {
+    else if (( 8 + (i)) == currentHr ) {
     blockHr[i].classList.remove("past");
     blockHr[i].classList.add("present");
+    console.log("present");
     }
     else {
     blockHr[i].classList.remove("past");
     blockHr[i].classList.remove("present")
     blockHr[i].classList.add("future")
+    console.log("future");
     }
 }
 
